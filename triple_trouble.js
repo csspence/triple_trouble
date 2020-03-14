9cf0b903110c658f82067844e12208c98199d7d2
Crew Spence <csspence123@gmail.com>
2020-03-14 07:49:20 -0500

    Wrote working solution for triple_trouble.

--- triple_trouble.js
+++ triple_trouble.js
@@ -1,6 +1,7 @@
 /*
 Triple Trouble
-Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the 
+first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
 
 E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
 
@@ -9,6 +10,11 @@ Note: You can expect all of the inputs to be the same length.
 */
 
 
-function tripleTrouble(one, two, three){
-  //Solution
+const tripleTrouble = (one, two, three) => {
+  let output = '';
+  for(let i = 0; i < one.length; i++) {
+    output += one[i] + two[i] + three[i];
+  }
+
+  return output;
  }
